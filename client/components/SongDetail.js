06927e3395cda2ce00bd446560fcf7cc326981ce
Taylor Pechacek <taylortpechacek@gmail.com>
2018-10-28 12:57:08 +1100

    wired up navigation to songDetail and back to songList

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
+import fetchSong from '../queries/fetchSong';
 
 class SongDetail extends Component {
   render() {
+    console.log(this.props);
+
+    // Deconstruct song
+    const { song } = this.props.data;
+
+    // Not yet a song available. Query is still pending.
+    if (!song) {
+      return <div>Loading...</div>;
+    }
+
     return (
       <div>
-        <h3>Song Detail</h3>
+        <Link to="/">Back</Link>
+        <h2>Song Detail</h2>
+        <h5>title: {song.title}</h5>
       </div>
     );
   }
 }
 
-export default SongDetail;
+export default graphql(fetchSong, {
+  options: props => {
+    return { variables: { id: props.params.id } };
+  }
+})(SongDetail);
+
+// One gotcha in here. we must stick an ID in here...
+// How does the react router take from params object and stick it into the ID?
+// No magic to push params.id into the fetchSong query. How do we get the variable into the
+// query?
+
+// We need to manually take this out of the this.props.params object and add it
+
+// This behaves different than this.props.mutate() where you cna pass in query variables.
+// This is very different than query
+
+// Queries are executed automatically but mutations are called manually.
+// So no clear place to pass query variables and stick them into a query when it's made.
